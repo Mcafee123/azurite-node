55fbc7cd16dbc896eb574874d4abb3a16def5081
Martin Affolter <martin@affolter.net>
2022-08-30 22:18:26 +0200

    skip api version check as there must be a bug (2021-06-08 instead of 2021-08-06)

--- docker/start.sh
+++ docker/start.sh
@@ -6,7 +6,7 @@
 . _build.sh
 
 echo "run"
-docker run -d -p 10000:10000 -p 10001:10001 -p 10002:10002 -p 3000:3000 --name $containername $containername
+docker run -d -p 10000:10000 -p 10001:10001 -p 10002:10002 -p 3000:3000 --name $containername $containername:$tagname
 
 echo "start express api in docker container"
 docker exec -d $containername sh -c 'cd /opt/azurite-node && node index.js'
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import { NextFunction, Request, Response } from 'express'
 import app from './App'
 import config from './config'
-import HttpException from './exceptions/HttpException'
 
 const port = process.env.PORT || config.port
 
