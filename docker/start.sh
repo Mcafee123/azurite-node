bb1c46c985448ce1b5b45aaeaf92e7363cc51de7
Martin Affolter <martin@affolter.net>
2023-01-20 10:59:31 +0100

    volume: example provided

--- docker/start.sh
+++ docker/start.sh
@@ -5,8 +5,15 @@
 . _config.sh
 . _build.sh
 
+localdatafolder="$(pwd)/../azurite-data"
+if test -f "$localdatafolder"; then
+  echo "create \"localdatafolder\""
+  mkdir "$localdatafolder"
+fi
+location="/data/newfolder"
+volume="$localdatafolder:/data"
 echo "run"
-docker run -d -e location="/data/newfolder" -p 10000:10000 -p 10001:10001 -p 10002:10002 -p 3000:3000 --name $containername $containername:$tagname
+docker run -d -e location="$location" -v "$volume" -p 10000:10000 -p 10001:10001 -p 10002:10002 -p 3000:3000 --name $containername $containername:$tagname
 # docker run -it -p 10000:10000 -p 10001:10001 -p 10002:10002 -p 3000:3000 --name $containername $containername:$tagname /bin/sh
 
 # echo "start express api in docker container"
